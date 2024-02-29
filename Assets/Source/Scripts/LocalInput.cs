bdf3db878241b70b1edaf0a34ef948d11f544a31
Nikolai Kochergin <Kocherginizer@gmail.com>
2024-02-29 15:37:45 +0300

    Add Simple Game

---
+++ Assets/Source/Scripts/LocalInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Source.Scripts
+{
+    public class LocalInput : MonoBehaviour
+    {
+        [SerializeField] private Player _player;
+
+        private void Update()
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+
+            _player.SetInput(h, v);
+        }
+    }
+}
---
+++ Assets/Source/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Source.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        private Vector3 _direction;
+
+        public void SetInput(float h, float v)
+        {
+            _direction.x = h;
+            _direction.z = v;
+        }
+
+        private void Update() => 
+            Move();
+
+        private void Move() => 
+            transform.position += _direction * (_speed * Time.deltaTime);
+    }
+}
